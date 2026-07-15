57056554a08ef787dd803505c8ab315fda7a0f2c
yeji <0123ye@naver.com>
2026-07-15 11:37:51 +0900

    yeji

---
+++ src/main/java/likelion14th/blog/TongtongdanApplication.java
@@ -0,0 +1,13 @@
+package likelion14th.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongtongdanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TongtongdanApplication.class, args);
+    }
+
+}
