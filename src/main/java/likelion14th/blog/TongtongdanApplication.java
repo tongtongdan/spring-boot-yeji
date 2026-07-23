a81c73b1959cfe8e0ab949b311e22e5e7ed25c77
yeji <0123ye@naver.com>
2026-07-23 11:55:29 +0900

    7월23ì스프링입문 실습2

--- src/main/java/likelion14th/blog/TongtongdanApplication.java
+++ src/main/java/likelion14th/blog/TongtongdanApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TongtongdanApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TongtongdanApplication.class, args);
+        System.out.println("SPRING"); //실행 확인용 출력
     }
-
 }
